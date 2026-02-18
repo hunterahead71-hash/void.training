15e529b49c5e95a59c73a1ed8476d63ca7b29165
hunterahead71-hash <hunterahead71@gmail.com>
2026-02-18 11:39:36 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Configuration
+const CONFIG = {
+    API_BASE_URL: 'https://mod-application-backend-production.up.railway.app',
+    TOTAL_QUESTIONS: 8,
+    PASSING_SCORE: 6
+};
+
+// Make config globally available
+window.CONFIG = CONFIG;
